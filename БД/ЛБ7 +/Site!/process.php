c82c2eef81f329fbd445410c986ab339b65be6a3
psrar <pavel.sima203@gmail.com>
2021-11-24 23:52:48 +0300

    5

---
+++ БД/ЛБ7 +/Site!/process.php
@@ -0,0 +1,7 @@
+<?php
+echo "Имя: " . $_POST["FName"] ."<br/>
+echo "Фамилия: " . $_POST["LName"] .";
+echo "Город: " . $_POST["City"];
+echo "<br/>";
+echo "Ваше пожелание:" . $_POST["Message"]
+?>
