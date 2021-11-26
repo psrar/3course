3f12070f849ccd3e2146431485d6d86cc059b010
psrar <pavel.sima203@gmail.com>
2021-11-26 11:36:08 +0300

    Исправленное ВКС

---
+++ ВКС/ПрактическиВКС/ЛБ6/VKS/ArrayUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKS
+{
+	static class ArrayUtils
+	{
+		static private readonly Random rnd = new();
+
+		public static void OutputArray<T>(T[] inArray)
+		{
+			inArray.ToList().ForEach(i => Console.WriteLine(i + " "));
+			Console.WriteLine();
+		}
+
+		public static int[] GenerateRandomNumericalArray(int length, int minIncl, int maxExcl) =>
+			Enumerable.Repeat(0, length).Select(i => rnd.Next(minIncl, maxExcl)).ToArray();
+	}
+}
---
+++ ВКС/ПрактическиВКС/ЛБ6/VKS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace VKS
+{
+	class Program
+	{
+		static void Main()
+		{
+			for(int i = 0; i < 5; i++)
+				ArrayUtils.OutputArray<int>(ArrayUtils.GenerateRandomNumericalArray(10, 0, 2));
+		}
+	}
+}
